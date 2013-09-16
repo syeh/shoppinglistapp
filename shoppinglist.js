1c822819c432a0ab55dfd6779299fd702668feb3
Shannon Yeh <shannon.yeh@me.com>
2013-09-16 16:35:29 -0400

    shoppinglist

--- shoppinglist.js
+++ shoppinglist.js
@@ -1,18 +1,25 @@
-
 $(document).ready(function () {
 	$("input[type='button']").on("click", function() {
 		var item = $("input[type='text']").val();
-		$("ol").append("<li style='display:none;'>"+ item+"<input type='checkbox'></li>");
+		   if ($('.entertext').val() == '') {
+     alert('Field is empty');
+     return false;
+     
+    }
+		$("ol").append("<li style='display:none;'>"+ item+"<input type='checkbox'></li><br />");
         $("li:last").show("clip");
 	});
 	$("input[type='text']").keyup(function(event) {
 
+
 	   if(event.keyCode == 13) {
 
 		  $("input[type='button']").click(); 
+		 
 	   }
    });
    $("ol").on("click", "input[type='checkbox']", function() {
 	   $(this).parent().remove();   
    });
+    
 });
